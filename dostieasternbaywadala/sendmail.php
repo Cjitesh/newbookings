dcc3b4ee16b7f15cb16bd850547fe8656bcb614b
Jitesh <cjitesh09@gmail.com>
2024-08-06 00:37:31 +0530

    changes

--- dostieasternbaywadala/sendmail.php
+++ dostieasternbaywadala/sendmail.php
@@ -8,8 +8,8 @@
           	'mobile' => '7900149466,9137692059',
 			//'mobile' => '7972844511',
            'countrycode' => $_POST['countrycode'],
-           'campaign' => '616',
-           'project' => '250',
+           'campaign' => '684',
+           'project' => '419',
            'toemail' => 'infojaiambe@gmail.com',
            'fromname' => 'New Bookings',
        );
@@ -22,8 +22,8 @@
           	'mobile' => '7900149466,9137692059',
 			//'mobile' => '7972844511',
            'countrycode' => $_POST['countrycode'],
-           'campaign' => '616',
-           'project' => '250',
+           'campaign' => '684',
+           'project' => '419',
            'toemail' => 'infojaiambe@gmail.com',
            'fromname' => 'New Bookings',
        );
@@ -36,8 +36,8 @@
           	'mobile' => '7900149466,9137692059',
 			//'mobile' => '7972844511',
            'countrycode' => $_POST['countrycode'],
-           'campaign' => '616',
-           'project' => '250',
+           'campaign' => '684',
+           'project' => '419',
            'toemail' => 'infojaiambe@gmail.com',
            'fromname' => 'New Bookings',
        );

f5fe87d47c710354e93faf7a8ef87993b0ae006c
Jitesh <cjitesh09@gmail.com>
2024-10-19 13:40:05 +0530

    godrejhorizondadarwadala

--- godrejhorizondadarwadala/sendmail.php
+++ godrejhorizondadarwadala/sendmail.php
@@ -1,44 +1,47 @@
 <?php
 	if($_POST['formtype'] == 'banner'){
 		$data = array(
+			'apiKey' => 'e9c33d8faae7e50260c045825ce52c86',
            'name' => $_POST['bannername'],
            'email' => $_POST['banneremail'],
            'contact' => $_POST['bannercontact'],
-           'path' => 'NB Godrej Horizon',
-          	'mobile' => '7900149466,9137692059,9930591919',
+           'path' => 'NB GODREJ HORIZON WADALA',
+          	'mobile' => '9930653494,9137692059',
 			//'mobile' => '7972844511',
            'countrycode' => $_POST['countrycode'],
            'campaign' => '616',
            'project' => '250',
-           'toemail' => 'infojaiambe@gmail.com',
+           'toemail' => 'shekhar.anush1@gmail.com',
            'fromname' => 'New Bookings',
        );
 	}elseif($_POST['formtype'] == 'sticky'){
 		$data = array(
+			'apiKey' => 'e9c33d8faae7e50260c045825ce52c86',
            'name' => $_POST['stickyname'],
            'email' => $_POST['stickyemail'],
            'contact' => $_POST['stickycontact'],
-           'path' => 'NB Godrej Horizon',
-          	'mobile' => '7900149466,9137692059,9930591919',
+           'path' => 'NB GODREJ HORIZON WADALA',
+          	'mobile' => '9930653494,9137692059',
 			//'mobile' => '7972844511',
            'countrycode' => $_POST['countrycode'],
            'campaign' => '616',
            'project' => '250',
-           'toemail' => 'infojaiambe@gmail.com',
+           'toemail' => 'shekhar.anush1@gmail.com',
            'fromname' => 'New Bookings',
        );
 	}elseif($_POST['formtype'] == 'modal'){
 		$data = array(
+			'apiKey' => 'e9c33d8faae7e50260c045825ce52c86',
            'name' => $_POST['modalname'],
            'email' => $_POST['modalemail'],
            'contact' => $_POST['modalcontact'],
-           'path' => 'NB Godrej Horizon',
-          	'mobile' => '7900149466,9137692059,9930591919',
+           'path' => 'NB GODREJ HORIZON WADALA',
+          	'mobile' => '9930653494,9137692059',
 			//'mobile' => '7972844511',
            'countrycode' => $_POST['countrycode'],
            'campaign' => '616',
            'project' => '250',
-           'toemail' => 'infojaiambe@gmail.com',
+           'toemail' => 'shekhar.anush1@gmail.com',
            'fromname' => 'New Bookings',
        );
 	}
